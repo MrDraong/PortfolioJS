83ba385ac4937d29418e32ab365cbd392a5c195e
Charles <charles.capiaux@compufirst.com>
2021-04-01 16:28:29 +0200

    Fichiers json

--- js/index.js
+++ js/index.js
@@ -1,54 +1,67 @@
-class Personne {
-    constructor(nom, prenom, age, description) {
+  class Personne {
+    constructor(nom, prenom, age, description, experience) {
       this.nom = nom;
       this.prenom = prenom;
-      this.age = age
+      this.age = age;
       this.description = description;
+      this.experience = experience;
     }
   }
 
-  let moi = new Personne("Capiaux", "Charles", "20 ans", "Développeur en alternance chez Compufirst");
-  let r_one = new Personne("Le Goff", "Erwan", "22 ans", "Egalement developpeur chez compufirst");
-  let aymeric = new Personne("Trouillard", "Aymeric", "vieux", "Un autre developpeur chez compufirst")
+  function loadPersonneJSON(unePersonne){
+    var res =  new Personne(unePersonne.firstname, unePersonne.lastname, unePersonne.age, unePersonne.job, unePersonne.experience);
+    return res;
+  }
+
+  let moi;
+  let aymeric;
+  let erwan;
 
   let tab = [];
   let cpt = 0;
 
-  tab.push(moi);
-  tab.push(r_one);
-  tab.push(aymeric);
+  $.getJSON("../js/charles.json").done(function(data){
+    moi = loadPersonneJSON(data);
+    tab.push(moi);
+    
+    changementPersonne();
+  });
 
-  changementPersonne();
+  $.getJSON("../js/erwan.json").done(function(data){
+    erwan = loadPersonneJSON(data);
+    tab.push(erwan);
+  });
+
+  $.getJSON("../js/aymeric.json").done(function(data){
+    aymeric = loadPersonneJSON(data);
+    tab.push(aymeric);
+  });
   
-
   $('#next').click(function() {
     cpt++;
     if(cpt == tab.length){
         cpt = 0;
     }
     changementPersonne();
-    });
+  });
 
-    $('#prev').click(function() {
-        if(cpt == 0){
-            cpt = tab.length;
-        }
+  $('#prev').click(function() {
+    if(cpt == 0){
+      cpt = tab.length;
+    }
         
-        cpt--;
-        changementPersonne();
-        });
-
-  $('#recur').click(function() {
-    recursive(10);
-    });
+    cpt--;
+    changementPersonne();
+  });
 
 function changementPersonne(){
     $('.card-title').html(tab[cpt].prenom + " " + tab[cpt].nom);
-        $('.card-subtitle').html(tab[cpt].description);
-        $('.card-text').html(tab[cpt].age);
-}
+    $('.card-subtitle').html(tab[cpt].description);
+    $('.card-text').html(tab[cpt].age);
 
-/*function loadPersonneJSON(monObjet){
-  let res = new Personne(monObjet.);
-  return res;
-}*/
+    var exp = "";
+    for(expe of tab[cpt].experience){
+      exp += expe.name + " du " + expe.date + " au " + expe.datefin + " : " + expe.description +"\n";
+    };
+    $('#pbord').html(exp);
+  }
